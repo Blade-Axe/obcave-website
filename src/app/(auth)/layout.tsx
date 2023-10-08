393cac0422536eb45f849add68d2e1a55ebe793b
BladeAxe <Madaxam@protonmail.com>
2023-10-08 12:23:29 +0100

    cosmetic changes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ interface AuthlayoutProps {
 
 const Authlayout: FC<AuthlayoutProps> = ({ children }) => {
   return (
-    <div className="bg-slate-200 w-full px-10 py-5 rounded-md sm:w-full sm:px-20 md:w-3/5 md:px-20 lg:w-2/5 lg:px-20 xl:w-1/3 xl:px-20">
+    <div className="bg-slate-100 w-full px-10 py-5 rounded-md sm:w-full sm:px-20 md:w-3/5 md:px-20 lg:w-2/5 lg:px-20 xl:w-1/3 xl:px-20">
       {children}
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-2xl">Home</h1>
+  return (
+    <div>
+      <h1 className="text-3xl">Home</h1>
+    </div>
+  )
 }
